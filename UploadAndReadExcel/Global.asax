67b6631e3d7404ec335512b7ff48b1d58a2b2089
terje <terje@MBP-WIN>
2016-05-12 21:10:13 +0200

    Add project files.

---
+++ UploadAndReadExcel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UploadAndReadExcel.MvcApplication" Language="C#" %>
---
+++ UploadAndReadExcel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UploadAndReadExcel.Startup))]
+namespace UploadAndReadExcel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
